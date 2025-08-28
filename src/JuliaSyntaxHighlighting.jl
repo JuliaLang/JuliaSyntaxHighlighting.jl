bfe9b16a79a637e3a8674b7745886981119cd35a
TEC <git@tecosaur.net>
2025-08-28 22:47:24 +0800

    Fetch all builtins from Base instead of Core
    This also gets us:
     - cglobal
     - ≡
     - invokelatest
     - donotdelete

--- src/JuliaSyntaxHighlighting.jl
+++ src/JuliaSyntaxHighlighting.jl
@@ -42,7 +42,7 @@ const SINGLETON_IDENTIFIERS = (:nothing, :missing)
 A set of identifiers that are defined in `Core` and a `Core.Builtin`.
 """
 const BUILTIN_FUNCTIONS =
-    Set([n for n in names(Core) if getglobal(Base, n) isa Core.Builtin])
+    Set([n for n in names(Base) if getglobal(Base, n) isa Core.Builtin])
 
 """
     HIGHLIGHT_FACES
